e41fabf52539ca89f0a900db86e44c3a8023df8a
taohui <htao@thoughtworks.com>
2022-09-17 18:33:47 +0800

    manage multiple state

--- src/Counter.js
+++ src/Counter.js
@@ -3,6 +3,7 @@ import {useSelector, useDispatch} from "react-redux";
 
 function Counter() {
   const counter = useSelector(state => state.counter)
+  const display = useSelector(state => state.display)
   const dispatch = useDispatch()
 
   // Action: code that causes an update to the state when something happens
@@ -12,20 +13,34 @@ function Counter() {
       })
   };
 
+  const increase = () => {
+    dispatch({
+        type: 'increase', amount: 5
+    })
+};
+
   const decrement = () => {
       dispatch({
           type: 'decrement'
       })
   }
 
+  const toggle = () => {
+    dispatch({
+        type: 'toggleCounter'
+    })
+}
+
   // View: the UI definition
   return (
     <div>
         <button style={{margin: '10px'}} onClick={increment}>Increment</button>
+        <button style={{margin: '10px'}} onClick={increase}>Increase</button>
         <button style={{margin: '10px'}} onClick={decrement}>Decrement</button>
-        <div>
+        <button style={{margin: '10px'}} onClick={toggle}>Toggle Counter</button>
+        {display && <div>
         Value: {counter} 
-        </div>
+        </div>}
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
 import {createStore} from 'redux';
 
-const counterReducer = (state = {counter: 0}, action)=> {
+const initialState = {counter: 0, display: true};
+const counterReducer = (state = initialState, action)=> {
     switch(action.type) {
         case 'increment':
-            return {counter: state.counter + 1}
+            return {
+                counter: state.counter + 1, 
+                display: state.display
+            }
+        case 'increase':
+            return {counter: state.counter + action.amount, display: state.display}    
         case 'decrement':
-            return {counter: state.counter - 1}
+            return {counter: state.counter - 1, display: state.display}
+        case 'toggleCounter':
+            return {
+                counter: state.counter, 
+                display: !state.display
+            }
         default:
             return state;
     }
